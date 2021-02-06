7a5666272d1183375a1c8943835cb2d0f98d87c1
Md.Mainul Islam <u1902141@student.cuet.ac.bd>
2021-02-06 22:00:13 +0600

    search and filter done as my own perspective

--- src/components/ProgramDirectory/ProgramDirectory.js
+++ src/components/ProgramDirectory/ProgramDirectory.js
@@ -207,13 +207,15 @@ const check4=(e)=>{
 
         if(phase && phase2 && phase3 && phase4){
 
+
             return(r.filter(r=>
                 (r.phase.indexOf(phase)>-1 ||
                 r.phase.indexOf(phase2)>-1 ||
                 r.phase.indexOf(phase3)>-1 ||
                 r.phase.indexOf(phase4)>-1) &&
-                (r.name.toLowerCase().indexOf(q)>-1 ||
-                r.name.indexOf(phase)>-1 ||
+                ( r.name.indexOf(phase)>-1 ||
+                r.name.toLowerCase().indexOf(q)>-1 ||
+               
                  r.category.indexOf(q)>-1   ||
                  r.category.toLowerCase().indexOf(q)>-1  ||
                  r.shortDescription.indexOf(q)>-1 ||
